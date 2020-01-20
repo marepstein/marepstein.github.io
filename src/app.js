da1949e55c2b9f654539267e6d3f43545d82c0f6
marepstein <marissaepstein@outlook.com>
2020-01-20 16:33:56 +0000

    react setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import BrowserRouter from 'react-router-dom'
+
+const App = () => {
+
+  return <div>Hello</div>
+	
+}
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
