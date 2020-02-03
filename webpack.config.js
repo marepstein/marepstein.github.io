ea94bd1c8314ae89feeb4458757947aeff4dcf6c
marepstein <marissaepstein@outlook.com>
2020-02-03 16:07:56 +0000

    merging

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.resolve(''),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/'
   },
